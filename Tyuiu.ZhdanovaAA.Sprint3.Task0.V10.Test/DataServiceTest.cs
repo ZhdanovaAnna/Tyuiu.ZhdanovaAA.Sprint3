d3fc55baad48b350f28ce420395ae8183f4f6256
ZhdanovaAnna <annaone01@mail.ru>
2023-10-21 21:01:04 +0500

    Task0

--- Tyuiu.ZhdanovaAA.Sprint3.Task0.V10.Test/DataServiceTest.cs
+++ Tyuiu.ZhdanovaAA.Sprint3.Task0.V10.Test/DataServiceTest.cs
@@ -16,7 +16,7 @@ namespace Tyuiu.ZhdanovaAA.Sprint3.Task0.V10.Test
 
             double res = ds.GetMultiplySeries(Value, startValue, stopValue);
 
-            Assert.AreEqual(0.033, ds.GetMultiplySeries(5, 1, 5));
+            Assert.AreEqual(0.033, res);
         }
     }
 }
